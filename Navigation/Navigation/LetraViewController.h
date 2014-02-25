bc46bec6b9fbb92186a7032c7d27bdb5388454b3
Isabela Mayumi R. Suitsu <isabelamayumirsuitsu@MacBook-Pro-de-Isabela.local>
2014-02-25 17:29:05 -0300

    Classe Letra, Dicionario e LetraViewController

---
+++ Navigation/Navigation/LetraViewController.h
@@ -0,0 +1,23 @@
+//
+//  LetraViewController.h
+//  Navigation
+//
+//  Created by Isabela Mayumi R. Suitsu  on 25/02/14.
+//  Copyright (c) 2014 Vinicius Miana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LetraViewController : UIViewController
+{
+//    NSString *letra;
+//    UIImage *imagem;
+//    NSString *palavra;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UILabel *labelPalavra;
+
+//-(id)initwith
+
+
+@end
---
+++ Navigation/Navigation/LetraViewController.m
@@ -0,0 +1,38 @@
+//
+//  LetraViewController.m
+//  Navigation
+//
+//  Created by Isabela Mayumi R. Suitsu  on 25/02/14.
+//  Copyright (c) 2014 Vinicius Miana. All rights reserved.
+//
+
+#import "LetraViewController.h"
+
+@interface LetraViewController ()
+
+@end
+
+@implementation LetraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
