07e2604a09f2ee46ed6ec91deea1529f2dbd6faa
Jurfest <jurfest.apps@gmail.com>
2014-02-26 09:12:58 -0300

    provisoriamente terminado

--- Navigation/Navigation/LetraAViewController.h
+++ Navigation/Navigation/LetraAViewController.h
@@ -1,13 +1,21 @@
 //
 //  LetraAViewController.h
-//  Navigation
+//  ios-navigation
 //
-//  Created by Vinicius Miana on 2/23/14.
-//  Copyright (c) 2014 Vinicius Miana. All rights reserved.
+//  Created by Diego Jurfest Ceccon on 25/02/14.
+//  Copyright (c) 2014 ___DIEGOJURFESTCECCON___. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
 @interface LetraAViewController : UIViewController
 
+@property (nonatomic, strong) UILabel *label;
+@property (nonatomic, strong) UITextField *x;
+@property (nonatomic, strong) AVSpeechSynthesizer *avspeech;
+@property (nonatomic, strong) AVSpeechUtterance *avutterance;
+@property (nonatomic, strong) NSArray *palavras;
+
 @end
+
